a41bb62770e40213e613da0247f673576687750d
Satyam Chaturvedi <satyam.chaturvedi@pls-solicitors.co.uk>
2026-06-25 15:02:08 +0530

    fix mongoose connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,8 +4,8 @@ import { config } from "./env.js";
 export const connectDB = async () => {
   try {
     if(!config.MONGO_URI) throw new Error('MONGO_URI is not set');
-    await mongoose.connect(config.MONGO_URI);
-    console.log("✅ Connected to Database");
+    const conn = await mongoose.connect(config.MONGO_URI);
+    console.log("✅ Connected to Database",conn.connection.host);
   } catch (error) {
     console.log("❌ Error connecting to MongoDB: ", error?.message || error);
     throw error;
